ee55b22ea14fcc4ee25cd4aab1f2c61116aa3299
oelsons <oelsonseg@hotmail.com>
2019-08-04 22:22:10 -0300

    Working | Added server config

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    AUTH0_DOMAIN: 'dev-oel.auth0.com',
+    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/',
+    MONGO_URI: 'mongodb+srv://admin:admin123@mean-pohgx.mongodb.net/test?retryWrites=true&w=majority'
+}
